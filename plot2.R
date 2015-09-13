6dc039fda8507ec880f6116c2e536b016bd841b0
cK <kamdemchristiane@gmail.com>
2015-09-13 23:45:59 +0200

    adding third plot

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+## For this example, weekdays are given in french
 ## Load data and transform in dataframe
 data <- read.table("household_power_consumption.txt", sep = ";", skip = 1, na.strings = "?", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 data1 <- data.frame(data)
